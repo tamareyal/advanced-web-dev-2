146d8c729197f614af143ee5c43134289f8c05f1
Adoram Shoval <s1adoram@gmail.com>
2025-12-19 14:12:59 +0200

    added posts and users schemas

---
+++ models/posts.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+export interface Post extends Document {
+    title: string;
+    content: string;
+    sender_id: mongoose.Schema.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const postSchema = new Schema<Post>({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    sender_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
+    }
+}, { timestamps: true });
+
+// We create a Model which controls the 'Posts' collection in MongoDB consisting of Post documents defined by postSchema
+const PostModel: Model<Post> = mongoose.model<Post>('Posts', postSchema);
+
+export default PostModel;
