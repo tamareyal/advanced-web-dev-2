6bd39e1402adbed2d45ad6a273aab3043a3f8fe0
Adoram Shoval <s1adoram@gmail.com>
2025-12-19 11:21:52 +0200

    feat: baseController for basic CRUD operations and implementation in the posts controller

---
+++ controllers/baseController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from 'express';
+import { Model } from 'mongoose';
+
+// BaseController for basic CRUD operations can be extended\overwritten by specific controllers
+class BaseController<T> {
+    model: Model<T>;
+
+    constructor(model: Model<T>) {
+        this.model = model;
+    }
+
+    getAll = async (req: Request, res: Response) => {
+        const filter = req.query;
+        try {
+            const data = await this.model.find(filter);
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(400).json({ message: error instanceof Error ? error.message : "Error" });
+        }
+    };
+
+    getById = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        try {
+            const data = await this.model.findById(id);
+            if (!data) {
+                return res.status(404).json({ message: "Resource not found" });
+            }
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json({ message: error instanceof Error ? error.message : "Error" });
+        }
+    };
+
+    // For many controllers this might need to be overwritten to add specific logic
+    // such as data verification, hashing passwords, etc to the given body.
+    create = async (req: Request, res: Response) => {
+        const body = req.body;
+        try {
+            const data = await this.model.create(body);
+            res.status(201).json(data);
+        } catch (error) {
+            res.status(400).json({ message: error instanceof Error ? error.message : "Error" });
+        }
+    };
+
+    // For many controllers this might need to be overwritten to add specific logic
+    // such as data verification, hashing passwords, etc to the given body.
+    update = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        const body = req.body;
+        try {
+            const data = await this.model.findByIdAndUpdate(id, body, { new: true });
+            if (!data) {
+                return res.status(404).json({ message: "Resource not found" });
+            }
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(400).json({ message: error instanceof Error ? error.message : "Error" });
+        }
+    };
+
+    delete = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        try {
+            const data = await this.model.findByIdAndDelete(id);
+            if (!data) {
+                return res.status(404).json({ message: "Resource not found" });
+            }
+            res.status(200).json({ message: "Deleted successfully" });
+        } catch (error) {
+            res.status(400).json({ message: error instanceof Error ? error.message : "Error" });
+        }
+    };
+}
+
+export default BaseController;
