2f0b1d048df3079a37185d85d8c8a2ff2edc5fba
tamareyal <tamareyal10@gmail.com>
2026-01-10 00:04:52 +0200

    override user put method

--- controllers/usersController.ts
+++ controllers/usersController.ts
@@ -1,3 +1,5 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import UserModel, { User } from '../models/users';
 import BaseController from './baseController';
 
@@ -5,6 +7,27 @@ class UsersController extends BaseController<User> {
     constructor() {
         super(UserModel);
     }
+
+    update = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        const body = req.body;
+
+        if (body.password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(body.password, salt);
+            body.password = hashedPassword;
+        }
+        
+        try {
+            const data = await this.model.findByIdAndUpdate(id, body, { new: true });
+            if (!data) {
+                return res.status(404).json({ message: "Resource not found" });
+            }
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(400).json({ message: error instanceof Error ? error.message : "Error" });
+        }
+    };
 }
 
 export default new UsersController();
