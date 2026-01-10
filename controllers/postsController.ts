f48c209da894f9a9a940b1d2239b7e21d3f0a93b
tamareyal <tamareyal10@gmail.com>
2026-01-10 22:17:38 +0200

    Add authorization middleware for verifying owner for actions like delete and update

--- controllers/postsController.ts
+++ controllers/postsController.ts
@@ -1,3 +1,5 @@
+import { AuthenticatedRequest } from '../middlewares/authMiddleware';
+import { Request, Response } from 'express';
 import PostsModel, { Post } from '../models/posts';
 import BaseController from './baseController';
 
@@ -5,6 +7,24 @@ class PostsController extends BaseController<Post> {
     constructor() {
         super(PostsModel);
     }
+
+    create = async (req: Request, res: Response) => {
+        const authReq = req as AuthenticatedRequest;
+        const body = req.body;
+        const senderId = authReq.userId;
+        if (!senderId) {
+            return res.status(401).json({ message: 'Unauthenticated' });
+        }
+        body.sender_id = senderId;
+
+        try {
+            const data = await this.model.create(body);
+            res.status(201).json(data);
+        } catch (error) {
+            res.status(400).json({ message: error instanceof Error ? error.message : "Error" });
+        }
+    }
+
 }
 
 export default new PostsController();
